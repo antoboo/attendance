f6f2eefefa9f946c34c62b09cf4617a6cf63f380
Antoinette Robert <antoinette.roberts85@yahoo.com>
2020-10-30 04:43:31 -0500

    Created Remote Database connection, Edited the connection info

--- db/conn.php
+++ db/conn.php
@@ -1,8 +1,18 @@
 <?php
-    $host = '127.0.0.1';
-    $db = 'attendance_db';
-    $user = 'root';
-    $pass = '';
+//     Development connection 
+//     $host = '127.0.0.1';
+//     $db = 'attendance_db';
+//     $user = 'root';
+//     $pass = '';
+//     $charset ='utf8mb4';
+
+
+
+//  Remote Database connection 
+    $host = 'sql9.freemysqlhosting.net';
+    $db = 'sql9373512';
+    $user = 'sql9373512';
+    $pass = 'bikFYZaLL7';
     $charset ='utf8mb4';
 
     $dsn = "mysql:host=$host; dbname=$db; charset=$charset";
--- edit.php
+++ edit.php
@@ -80,8 +80,8 @@
             <small id="phoneHelp" class="form-text text-muted">We'll never share your phone number with anyone else.</small>
         </div>
 
-        <button type="submit"  name = "submit" class="btn btn-secondary">Save Changes</button>
-        <a href = viewrecords.php  class="btn btn-info float=right"> Back to List </a>   
+        <button type="submit"  name = "submit" class="btn btn-success">Save Changes</button>
+        <a href = viewrecords.php  class="btn btn-primary float-right"> Back to List </a>   
 
     </form>
 
