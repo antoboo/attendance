45fef9c919f45848b4724c8be48dbf0d859179e2
Antoinette Robert <antoinette.roberts85@yahoo.com>
2020-11-06 05:14:33 -0500

    Connection amendments

--- db/conn.php
+++ db/conn.php
@@ -1,19 +1,19 @@
 <?php
 //     Development connection 
-    $host = '127.0.0.1';
-    $db = 'attendance_db';
-    $user = 'root';
-    $pass = '';
-    $charset ='utf8mb4';
+    // $host = '127.0.0.1';
+    // $db = 'attendance_db';
+    // $user = 'root';
+    // $pass = '';
+    // $charset ='utf8mb4';
 
 
 
 //  Remote Database connection 
-//     $host = 'sql9.freemysqlhosting.net';
-//     $db = 'sql9373512';
-//     $user = 'sql9373512';
-//     $pass = 'bikFYZaLL7';
-//     $charset ='utf8mb4';
+    $host = 'sql9.freemysqlhosting.net';
+    $db = 'sql9373512';
+    $user = 'sql9373512';
+    $pass = 'bikFYZaLL7';
+    $charset ='utf8mb4';
 
     $dsn = "mysql:host=$host; dbname=$db; charset=$charset";
 
