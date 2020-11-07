943a4b1d69e8d5552ad631092a2a2bad90d02f08
Antoinette Robert <antoinette.roberts85@yahoo.com>
2020-11-07 15:14:31 -0500

    Email sender third party - changed from SendGrid to ElasticEmail

--- sendemail.php
+++ sendemail.php
@@ -3,13 +3,13 @@
 
     class SendEmail{
         public static function SendMail($to, $subject, $content){
-            $key = "6B9037BE4997DCD3112515C6180075D13CF7BFBF3EC6356B4B69776116F622063558D7513ECD08E8B61280E8D4BDF88C";
-            //$key = '086E76797EC570A5A10EC20A7762C67EC89581EF2BBDFBA16E833C6D84C9F78F4819424550AEECFE8600A83B68881FF6';
+        
+            $key = '93DF41D594099193F5BA5FC0272A4E191323A223D1D9F33CF27C00C36239AD478E3726A645359148B1FB947551C6F4CA';
             $url = 'https://api.elasticemail.com/v2/email/send';
 
             try {
-
-                $email = array('from' => 'antoinette.roberts85@yahoo.com',
+        
+                $email = array('from'=> "annntoboo@gmail.com",
                 'fromName' => 'Antoinette',
                 'apikey' => $key,
                 'subject' => $subject,
@@ -17,7 +17,6 @@
                 'bodyHtml' => $content,
                 'bodyText' => $content,
                 'isTransactional' => false);
-                
                 $ch = curl_init();
                 curl_setopt_array($ch, array(
                     CURLOPT_URL => $url,
@@ -52,7 +51,7 @@
       //  $key = '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';
 
       //  $email = new \SendGrid\Mail\Mail();
-        // $email->setFrom("antoinette.roberts@yahoo.com","Antoinette Roberts-Graham");
+        // $email->setFrom("antoinette.roberts85@yahoo.com","Antoinette Roberts-Graham");
         // $email->setSubject($subject);
         // $email->addTo($to);
         // $email->addContent("text/plain",$content);
